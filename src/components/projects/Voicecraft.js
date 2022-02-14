89c3b304ac714baad1c57175146877e879c3a581
Kai <83799062+gr812b@users.noreply.github.com>
2022-02-14 01:51:33 -0500

    yeos

--- src/components/projects/Voicecraft.js
+++ src/components/projects/Voicecraft.js
@@ -9,8 +9,13 @@ const Voicecraft = (props) => {
             <div className="project-description">
                 <p>
                     Voicecraft is a voice recognition application that allows users to record and save their voice.
-                    It is built with React, Express, MongoDB, and Node.js. It is deployed on Heroku.
+                    It is built with PySimpleGui and Python.
+
+                    Built for MacHacks 2022, this application allows you to interface with your computer via audio input only. Using an easy-to-use configuration gui, and state of the art voice recognition technology, playing games such as minecraft is easier than ever before.
                 </p>
+            </div>
+            <div className="project-images">
+                <img src="https://i.imgur.com/Em5IaYu.png" alt="Voicecraft" />
             </div>
             <div className="project-links">
                 <a href="https://github.com/gr812b/Voicecraft" target="_blank" rel="noopener noreferrer">GitHub</a>
